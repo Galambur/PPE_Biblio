cc6981a51ef0d700391629498bbed409e6a8bce3
Galambur <gaelle.derambure@epsi.fr>
2020-10-20 11:32:44 +0200

    connexion ok

---
+++ php/client/actions/connexion.php
@@ -0,0 +1,29 @@
+<header>
+    <?php
+    require_once ("../../../php/fonctions/head.php");
+    ?>
+
+</header>
+<body>
+
+<h2>Se connecter</h2>
+
+<br>
+
+<form action="login.php" method="post">
+    <label for="email">email :</label>
+    <input type="text" name="email"  value=""/><br /><br />
+
+    <label for="mdp">Mot de passe :</label>
+    <input type="text" name="mdp"  value=""/><br /><br />
+
+    <input type="submit" class="button_form" value="Valider"/>
+</form>
+
+
+
+
+
+
+</body>
+</html>
